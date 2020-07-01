27b406c651f290d7364119ee9d66a4060da9b7b2
occiandiaali <ocean.diaali@gmail.com>
2020-07-01 16:17:01 +0100

    add methods to update search and select item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation/ngx';
 
 declare var google;
@@ -10,11 +10,77 @@ declare var google;
 })
 export class Tab1Page implements AfterViewInit {
 
+  autocomplete: any;
+  GoogleAutocomplete: any;
+  GooglePlaces: any;
+  geocoder: any
+  autocompleteItems: any;
+  nearbyItems: any = new Array<any>();
+  loading: any;
+
   options: GeolocationOptions;
   currentPos: Geoposition;
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  constructor(private geolocation: Geolocation) {}
+
+  constructor(private geolocation: Geolocation, public zone: NgZone) {
+    this.geocoder = new google.maps.Geocoder;
+    let elem = document.createElement("div")
+    this.GooglePlaces = new google.maps.places.PlacesService(elem);
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = {
+      input: ''
+    };
+    this.autocompleteItems = [];
+
+  } // ctr
+
+  updateSearchResults(){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = null;
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+      (predictions, status) => {
+        this.autocompleteItems = [];
+        if(predictions) {
+          this.zone.run(() => {
+            predictions.forEach((prediction) => {
+              this.autocompleteItems.push(prediction);
+            });
+          });
+        } else {
+          console.log(`${status}`);
+        }
+    });
+  } // update search results
+
+
+  selectSearchResult(item){
+    // this.loading.present();
+    // this.autocompleteItems = [];
+    console.log("Selected: " + item.toString());
+    this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
+      if(status === 'OK' && results[0]){
+        this.autocompleteItems = [];
+        this.GooglePlaces.nearbySearch({
+          location: results[0].geometry.location,
+          radius: '1600',
+          types: ['address'], //check other types here https://developers.google.com/places/web-service/supported_types
+           key: 'API_KEY'
+        }, (near_places) => {
+          this.zone.run(() => {
+            this.nearbyItems = [];
+            for (var i = 0; i < near_places.length; i++) {
+              this.nearbyItems.push(near_places[i]);
+            }
+            // this.loading.dismiss();
+          });
+        })
+      }
+    })
+  }
 
   getUserPosition() {
     this.options = {
