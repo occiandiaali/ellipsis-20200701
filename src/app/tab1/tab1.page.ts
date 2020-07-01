64f3f78596ed739d6fbc6992649c973846d1458b
occiandiaali <ocean.diaali@gmail.com>
2020-07-01 14:49:20 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation/ngx';
+
+declare var google;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
 
-  constructor() {}
+  options: GeolocationOptions;
+  currentPos: Geoposition;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  constructor(private geolocation: Geolocation) {}
+
+  getUserPosition() {
+    this.options = {
+        enableHighAccuracy : true
+    };
+
+    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+        this.currentPos = pos;      
+        console.log(pos);
+
+        this.addMap(pos.coords.latitude, pos.coords.longitude);
+
+    },(err : PositionError)=>{
+        console.log("error : " + err.message);
+    });
+} // get user pos
+
+ngAfterViewInit() {
+  this.getUserPosition();
+} // after view init
+
+addMap(lat,long){
+
+  let latLng = new google.maps.LatLng(lat, long);
+
+  let mapOptions = {
+  center: latLng,
+  zoom: 15,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  this.addMarker();
 
 }
+
+addMarker(){
+
+  let marker = new google.maps.Marker({
+  map: this.map,
+  animation: google.maps.Animation.DROP,
+  position: this.map.getCenter()
+  });
+
+  let content = "<p>This is your current position !</p>";          
+  let infoWindow = new google.maps.InfoWindow({
+  content: content
+  });
+
+  google.maps.event.addListener(marker, 'click', () => {
+  infoWindow.open(this.map, marker);
+  });
+
+}
+
+
+} // class
